4d964ec80bff0ece9cad647f3fee1708edc4f012
Kenneth Jung <ohikendoit@gmail.com>
2021-08-24 09:08:52 +0300

    Initial commit - Based on Samirsaci.com

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, jsonify
+import json
+
+app = Flask(__name__)
+
+#Import DataFrame
+PATH_IN = r'static\data\miserables.json'
+
+#Routing to define URL
+@app.route('/')
+def index():
+    return render_template('matrix.html')
+
+#Post json
+@app.route('/get-json', methods=['GET', 'POST'])
+def get_json():
+    #Import Data
+    with open(PATH_IN) as f:
+        json_to = json.load(f)
+    return jsonify(json_to)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
