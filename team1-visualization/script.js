0d15501f0f8adf23f96c7a8f8a0f6728f08b8af7
Kenneth Jung <ohikendoit@gmail.com>
2021-08-27 14:55:18 +0300

    Progress commit

--- team1-visualization/script.js
+++ team1-visualization/script.js
@@ -37,8 +37,8 @@ var yAxis = d3.svg.axis()
 
 var line = d3.svg.line()
     .interpolate("cardinal")
-    .x(function(d) { return x(+d.day); })
-    .y(function(d) { return y(+d.people); });
+    .x(function(d) { return x(+d.year); })
+    .y(function(d) { return y(+d.similarity); });
 
 var svg = d3.select("#line1").append("svg")
     .attr("id","travel-chart")
@@ -54,167 +54,169 @@ var data = [
   {
     "year": 2007,
     "DCED": 0.020291951,
-    "UNDP-Private-Sector-Strategy-2007": 0.0361934446,
-    "UNDP-Private-Sector-Strategy-2012": 0.0314169125,
-    "UNDP-Private-Sector-Strategy-2016": 0.0006714684,
-    "UNDP-Private-Sector-Strategy-2018": 0.0350867082,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0221212199
+    "UNDP_Private_Sector_Strategy_2007": 0.0361934446,
+    "UNDP_Private_Sector_Strategy_2012": 0.0314169125,
+    "UNDP_Private_Sector_Strategy_2016": 0.0006714684,
+    "UNDP_Private_Sector_Strategy_2018": 0.0350867082,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0221212199
   },
   {
     "year": 2008,
     "DCED": 0.022419527,
-    "UNDP-Private-Sector-Strategy-2007": 0.0416624941,
-    "UNDP-Private-Sector-Strategy-2012": 0.0329868813,
-    "UNDP-Private-Sector-Strategy-2016": 0.0004725325,
-    "UNDP-Private-Sector-Strategy-2018": 0.0401262674,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0246019848
+    "UNDP_Private_Sector_Strategy_2007": 0.0416624941,
+    "UNDP_Private_Sector_Strategy_2012": 0.0329868813,
+    "UNDP_Private_Sector_Strategy_2016": 0.0004725325,
+    "UNDP_Private_Sector_Strategy_2018": 0.0401262674,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0246019848
   },
   {
     "year": 2009,
     "DCED": 0.0237735138,
-    "UNDP-Private-Sector-Strategy-2007": 0.0352088052,
-    "UNDP-Private-Sector-Strategy-2012": 0.0321056716,
-    "UNDP-Private-Sector-Strategy-2016": 0.0019624646,
-    "UNDP-Private-Sector-Strategy-2018": 0.0391119149,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0242665109
+    "UNDP_Private_Sector_Strategy_2007": 0.0352088052,
+    "UNDP_Private_Sector_Strategy_2012": 0.0321056716,
+    "UNDP_Private_Sector_Strategy_2016": 0.0019624646,
+    "UNDP_Private_Sector_Strategy_2018": 0.0391119149,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0242665109
   },
   {
     "year": 2010,
     "DCED": 0.0238007571,
-    "UNDP-Private-Sector-Strategy-2007": 0.037147467,
-    "UNDP-Private-Sector-Strategy-2012": 0.0324884883,
-    "UNDP-Private-Sector-Strategy-2016": 0.0008416573,
-    "UNDP-Private-Sector-Strategy-2018": 0.0399596994,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0239247314
+    "UNDP_Private_Sector_Strategy_2007": 0.037147467,
+    "UNDP_Private_Sector_Strategy_2012": 0.0324884883,
+    "UNDP_Private_Sector_Strategy_2016": 0.0008416573,
+    "UNDP_Private_Sector_Strategy_2018": 0.0399596994,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0239247314
   },
   {
     "year": 2011,
     "DCED": 0.0231831599,
-    "UNDP-Private-Sector-Strategy-2007": 0.0363701122,
-    "UNDP-Private-Sector-Strategy-2012": 0.0324208841,
-    "UNDP-Private-Sector-Strategy-2016": 0.0010064769,
-    "UNDP-Private-Sector-Strategy-2018": 0.0422763746,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0253848753
+    "UNDP_Private_Sector_Strategy_2007": 0.0363701122,
+    "UNDP_Private_Sector_Strategy_2012": 0.0324208841,
+    "UNDP_Private_Sector_Strategy_2016": 0.0010064769,
+    "UNDP_Private_Sector_Strategy_2018": 0.0422763746,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0253848753
   },
   {
     "year": 2012,
     "DCED": 0.023650112,
-    "UNDP-Private-Sector-Strategy-2007": 0.0342823922,
-    "UNDP-Private-Sector-Strategy-2012": 0.0321699101,
-    "UNDP-Private-Sector-Strategy-2016": 0.0012656741,
-    "UNDP-Private-Sector-Strategy-2018": 0.0412159356,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0251234786
+    "UNDP_Private_Sector_Strategy_2007": 0.0342823922,
+    "UNDP_Private_Sector_Strategy_2012": 0.0321699101,
+    "UNDP_Private_Sector_Strategy_2016": 0.0012656741,
+    "UNDP_Private_Sector_Strategy_2018": 0.0412159356,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0251234786
   },
   {
     "year": 2013,
     "DCED": 0.0250250211,
-    "UNDP-Private-Sector-Strategy-2007": 0.0346085964,
-    "UNDP-Private-Sector-Strategy-2012": 0.0329898914,
-    "UNDP-Private-Sector-Strategy-2016": 0.0009485515,
-    "UNDP-Private-Sector-Strategy-2018": 0.0419427593,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0254680778
+    "UNDP_Private_Sector_Strategy_2007": 0.0346085964,
+    "UNDP_Private_Sector_Strategy_2012": 0.0329898914,
+    "UNDP_Private_Sector_Strategy_2016": 0.0009485515,
+    "UNDP_Private_Sector_Strategy_2018": 0.0419427593,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0254680778
   },
   {
     "year": 2014,
     "DCED": 0.0255694233,
-    "UNDP-Private-Sector-Strategy-2007": 0.031896028,
-    "UNDP-Private-Sector-Strategy-2012": 0.0311893811,
-    "UNDP-Private-Sector-Strategy-2016": 0.0015004914,
-    "UNDP-Private-Sector-Strategy-2018": 0.0423540989,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0251941554
+    "UNDP_Private_Sector_Strategy_2007": 0.031896028,
+    "UNDP_Private_Sector_Strategy_2012": 0.0311893811,
+    "UNDP_Private_Sector_Strategy_2016": 0.0015004914,
+    "UNDP_Private_Sector_Strategy_2018": 0.0423540989,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0251941554
   },
   {
     "year": 2015,
     "DCED": 0.0258569295,
-    "UNDP-Private-Sector-Strategy-2007": 0.0304321291,
-    "UNDP-Private-Sector-Strategy-2012": 0.0322096777,
-    "UNDP-Private-Sector-Strategy-2016": 0.0019243729,
-    "UNDP-Private-Sector-Strategy-2018": 0.0437096514,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0269645161
+    "UNDP_Private_Sector_Strategy_2007": 0.0304321291,
+    "UNDP_Private_Sector_Strategy_2012": 0.0322096777,
+    "UNDP_Private_Sector_Strategy_2016": 0.0019243729,
+    "UNDP_Private_Sector_Strategy_2018": 0.0437096514,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0269645161
   },
   {
     "year": 2016,
     "DCED": 0.0257385951,
-    "UNDP-Private-Sector-Strategy-2007": 0.0283150894,
-    "UNDP-Private-Sector-Strategy-2012": 0.0331813971,
-    "UNDP-Private-Sector-Strategy-2016": 0.0018711434,
-    "UNDP-Private-Sector-Strategy-2018": 0.0459398953,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0283019923
+    "UNDP_Private_Sector_Strategy_2007": 0.0283150894,
+    "UNDP_Private_Sector_Strategy_2012": 0.0331813971,
+    "UNDP_Private_Sector_Strategy_2016": 0.0018711434,
+    "UNDP_Private_Sector_Strategy_2018": 0.0459398953,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0283019923
   },
   {
     "year": 2017,
     "DCED": 0.0273287336,
-    "UNDP-Private-Sector-Strategy-2007": 0.0285936739,
-    "UNDP-Private-Sector-Strategy-2012": 0.0345854378,
-    "UNDP-Private-Sector-Strategy-2016": 0.0024717865,
-    "UNDP-Private-Sector-Strategy-2018": 0.0489208225,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0302585411
+    "UNDP_Private_Sector_Strategy_2007": 0.0285936739,
+    "UNDP_Private_Sector_Strategy_2012": 0.0345854378,
+    "UNDP_Private_Sector_Strategy_2016": 0.0024717865,
+    "UNDP_Private_Sector_Strategy_2018": 0.0489208225,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0302585411
   },
   {
     "year": 2018,
     "DCED": 0.029152143,
-    "UNDP-Private-Sector-Strategy-2007": 0.0272947641,
-    "UNDP-Private-Sector-Strategy-2012": 0.0366151984,
-    "UNDP-Private-Sector-Strategy-2016": 0.0028014159,
-    "UNDP-Private-Sector-Strategy-2018": 0.0528508123,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0329337998
+    "UNDP_Private_Sector_Strategy_2007": 0.0272947641,
+    "UNDP_Private_Sector_Strategy_2012": 0.0366151984,
+    "UNDP_Private_Sector_Strategy_2016": 0.0028014159,
+    "UNDP_Private_Sector_Strategy_2018": 0.0528508123,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0329337998
   },
   {
     "year": 2019,
     "DCED": 0.0288776696,
-    "UNDP-Private-Sector-Strategy-2007": 0.0286180593,
-    "UNDP-Private-Sector-Strategy-2012": 0.0384871664,
-    "UNDP-Private-Sector-Strategy-2016": 0.0024549477,
-    "UNDP-Private-Sector-Strategy-2018": 0.0552202705,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0347893662
+    "UNDP_Private_Sector_Strategy_2007": 0.0286180593,
+    "UNDP_Private_Sector_Strategy_2012": 0.0384871664,
+    "UNDP_Private_Sector_Strategy_2016": 0.0024549477,
+    "UNDP_Private_Sector_Strategy_2018": 0.0552202705,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0347893662
   },
   {
     "year": 2020,
     "DCED": 0.0302939846,
-    "UNDP-Private-Sector-Strategy-2007": 0.0280460855,
-    "UNDP-Private-Sector-Strategy-2012": 0.0387403486,
-    "UNDP-Private-Sector-Strategy-2016": 0.0023483863,
-    "UNDP-Private-Sector-Strategy-2018": 0.0563184778,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0358705131
+    "UNDP_Private_Sector_Strategy_2007": 0.0280460855,
+    "UNDP_Private_Sector_Strategy_2012": 0.0387403486,
+    "UNDP_Private_Sector_Strategy_2016": 0.0023483863,
+    "UNDP_Private_Sector_Strategy_2018": 0.0563184778,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0358705131
   },
   {
     "year": 2021,
     "DCED": 0.0313098488,
-    "UNDP-Private-Sector-Strategy-2007": 0.0308223976,
-    "UNDP-Private-Sector-Strategy-2012": 0.0370627459,
-    "UNDP-Private-Sector-Strategy-2016": 0.0021356228,
-    "UNDP-Private-Sector-Strategy-2018": 0.0540418456,
-    "UNDP-Private-Sector-Strategy-2018-2022": 0.0348219597
+    "UNDP_Private_Sector_Strategy_2007": 0.0308223976,
+    "UNDP_Private_Sector_Strategy_2012": 0.0370627459,
+    "UNDP_Private_Sector_Strategy_2016": 0.0021356228,
+    "UNDP_Private_Sector_Strategy_2018": 0.0540418456,
+    "UNDP_Private_Sector_Strategy_2018_2022": 0.0348219597
   }
 ];
 
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "day"; }));
+  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
   
   data.forEach(function(d) {
-    d.day = +d.day;
+    d.year = +d.year;
 
   });
   reports = color.domain().map(function(name) {
     return {
       name: name,
       values: data.map(function(d) {
-        return {day: d.day, people: +d[name] };
+        return {year: d.year, similarity: +d[name] };
       })
     };
   }); 
 
-var bus = reports[0];
-var plane = transports[1];
-var train = transports[2];
-var car = transports[3];
+var DCED = reports[0];
+var UNDP_Private_Sector_Strategy_2007 = reports[1];
+var UNDP_Private_Sector_Strategy_2012 = reports[2];
+var UNDP_Private_Sector_Strategy_2016 = reports[3];
+var UNDP_Private_Sector_Strategy_2018 = reports[4];
+var UNDP_Private_Sector_Strategy_2018_2022 = reports[5];
 
-  x.domain(d3.extent(data, function(d) { return d.day; }))
+  x.domain(d3.extent(data, function(d) { return d.year; }))
     .range([0, width]);
    // .clamp(true);
 
   y.domain([
-    d3.min(transports, function(c) { return d3.min(c.values, function(v) { return v.people; }); }),
-    d3.max(transports, function(c) { return d3.max(c.values, function(v) { return v.people; }); })
+    d3.min(reports, function(c) { return d3.min(c.values, function(v) { return v.similarity; }); }),
+    d3.max(reports, function(c) { return d3.max(c.values, function(v) { return v.similarity; }); })
   ]);
 svg.append("g")
       .attr("class", "x axis")
@@ -222,7 +224,7 @@ svg.append("g")
       .call(xAxis);
 
   var city = svg.selectAll(".city")
-      .data(transports)
+      .data(reports)
       .enter().append("g")
       .attr("class", "city");
 
@@ -292,10 +294,12 @@ function mouseMove() {
             value   = Math.round(x.invert(mouseX)),
             valMap = value-1;  //map day - 1 for proper match (since array indexing starts at 0)
           if (value > 0) {  
-            var dayBus = data[valMap].bus;
-            var dayTrain = data[valMap].train;
-            var dayPlane = data[valMap].plane;
-            var dayCar = data[valMap].car;
+            var yearDCED = data[valMap].DCED;
+            var yearUNDP_Private_Sector_Strategy_2007 = data[valMap].UNDP_Private_Sector_Strategy_2007;
+            var yearUNDP_Private_Sector_Strategy_2012 = data[valMap].UNDP_Private_Sector_Strategy_2012;
+            var yearUNDP_Private_Sector_Strategy_2016 = data[valMap].UNDP_Private_Sector_Strategy_2016;
+            var yearUNDP_Private_Sector_Strategy_2018 = data[valMap].UNDP_Private_Sector_Strategy_2018;
+            var yearUNDP_Private_Sector_Strategy_2018_2022 = data[valMap].UNDP_Private_Sector_Strategy_2018_2022;
           }
         //** Display tool tip
         
@@ -320,18 +324,22 @@ function mouseMove() {
           toolTip.style("left", (mouseX - 140 + "px"));
   }
 //get daily values and print
-            $(".textB").text(dayBus.toLocaleString()),
-            $(".textP").text(dayPlane.toLocaleString()),
-            $(".textT").text(dayTrain.toLocaleString()),
-            $(".textC").text(dayCar.toLocaleString());
+            $(".textB").text(yearDCED.toLocaleString()),
+            $(".textP").text(yearUNDP_Private_Sector_Strategy_2007.toLocaleString()),
+            $(".textT").text(yearUNDP_Private_Sector_Strategy_2012.toLocaleString()),
+            $(".textC").text(yearUNDP_Private_Sector_Strategy_2016.toLocaleString());
+            $(".textC").text(yearUNDP_Private_Sector_Strategy_2018.toLocaleString());
+            $(".textC").text(yearUNDP_Private_Sector_Strategy_2018_2022.toLocaleString());
     }//end mousemove
 
 function mouseOut() {
   toolTip.style('visibility', 'hidden');
-           var totalBus = 0,
-              totalTrain = 0,
-              totalPlane = 0,
-              totalCar = 0;//reset values
+           var totalDCED = 0,
+              totalUNDP_Private_Sector_Strategy_2007 = 0,
+              totalUNDP_Private_Sector_Strategy_2012 = 0,
+              totalUNDP_Private_Sector_Strategy_2016 = 0,
+              totalUNDP_Private_Sector_Strategy_2018 = 0,
+              totalUNDP_Private_Sector_Strategy_2018_2022 = 0;//reset values
 }
 
 });
